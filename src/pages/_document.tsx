dca51bac1667f6ea50a8e4d5347cc5f59ec6794f
pung8146 <pung8146@naver.com>
2025-04-09 07:37:51 +0900

    상훈 : feat: 글로벌 헤더 추가 및 문서 언어 설정 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
+// 기존 프로젝트의 index.html 파일을 수정하는 것과 비슷한 역할을 합니다.
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="kr">
       <Head />
       <body>
         <Main />
