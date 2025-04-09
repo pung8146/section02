14634f20eed58b531bacc8028107922ba3f293b1
pung8146 <pung8146@naver.com>
2025-04-10 07:29:34 +0900

    feat: 책 페이지 추가 및 404 페이지 구현

--- src/pages/book/[[...id]].tsx
+++ src/pages/book/[[...id]].tsx
@@ -2,10 +2,11 @@ import { useRouter } from "next/router";
 
 export default function Page() {
   const router = useRouter();
+  const { id } = router.query;
   console.log("router", router);
   return (
     <div>
-      <h1>Book</h1>
+      <h1>Book {id}</h1>
       <p>This is the book page.</p>
     </div>
   );
