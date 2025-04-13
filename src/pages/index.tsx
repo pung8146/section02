b1da0ad97a40ae9e610179000e1d948d8aa402b2
pung8146 <pung8146@naver.com>
2025-04-14 06:53:18 +0900

    홈페이지에 CSS 모듈을 적용하여 스타일 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+// CSS 모듈 사용
+import style from "./index.module.css";
+
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Next.js!</h1>
-      <p>This is a simple Next.js application.</p>
+      <h1 className={style.h1}>Welcome to Next.js!</h1>
+      <p className={style.p}>This is a simple Next.js application.</p>
     </div>
   );
 }
