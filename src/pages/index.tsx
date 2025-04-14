b1cc77681dfb92b173a1ee770c547b8c070c6a1f
pung8146 <pung8146@naver.com>
2025-04-15 07:36:47 +0900

    홈페이지 레이아웃을 개선하고, 검색 페이지에 도서 목록을 추가하며, 불필요한 도서 페이지를 삭제함

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 // CSS 모듈 사용
+import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
+import { ReactNode } from "react";
+import books from "@/mock/books.json";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={style.h1}>Welcome to Next.js!</h1>
-      <p className={style.p}>This is a simple Next.js application.</p>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books?.map((book) => (
+          <BookItem key={book.id} book={book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books?.map((book) => (
+          <BookItem key={book.id} book={book} />
+        ))}
+      </section>
     </div>
   );
 }
+
+Home.getLayout = function getLayout(page: ReactNode) {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
